6d377c2ca22feff855b29c09271d9120c12afd98
Frank Carpio <frankcarpio8@gmail.com>
2022-09-25 00:41:48 -0500

    build

--- store/store.ts
+++ store/store.ts
@@ -1,14 +1,9 @@
-import {combineReducers, configureStore, getDefaultMiddleware, Store} from "@reduxjs/toolkit";
+import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 import createSagaMiddleware from 'redux-saga';
 import movieReducer from "./movie/reducer";
-import {Task} from "@redux-saga/types";
 import movieSagas from "./movie/saga";
 import logger from "redux-logger";
 import {MovieState} from "./movie/types";
-
-export interface SagaStore extends Store {
-  movieTask?: Task;
-}
 
 const rootReducer = combineReducers({movieReducer});
 
@@ -21,7 +16,7 @@ const middleware = [...getDefaultMiddleware(), sagaMiddleware, logger];
 
 const makeStore: any = () => {
   const store = configureStore({reducer: rootReducer, middleware, devTools: Boolean(process.env.REACT_APP_DEV_TOOLS)});
-  (store as any).movieTask = sagaMiddleware.run(movieSagas);
+  sagaMiddleware.run(movieSagas);
   return store;
 
 }
